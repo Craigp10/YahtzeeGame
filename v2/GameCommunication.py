cbe15194c8aeaa3a837930f7313260b90a237620
Craig <CMPeoples3@gmail.com>
2022-03-01 10:44:32 -0800

    added game communication object

---
+++ v2/GameCommunication.py
@@ -0,0 +1,12 @@
+
+
+class GameCommunication:
+  def __init__(self):
+    self.self= self
+
+  
+  def printStatement(self, statment):
+    print(statment)
+
+  def inputStatment(self, statment):
+    return input(statment)
