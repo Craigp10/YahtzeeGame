034186799827b012f4d70f2b707b5a93a574a976
Craig <CMPeoples3@gmail.com>
2022-03-01 22:27:35 -0800

    game communication class

--- v2/GameCommunication.py
+++ v2/GameCommunication.py
@@ -5,8 +5,23 @@ class GameCommunication:
     self.self= self
 
   
-  def printStatement(self, statment):
-    print(statment)
+  def printStatement(self, statement: str) -> None:
+    print(statement)
 
-  def inputStatment(self, statment):
-    return input(statment)
+  def stringInputstatement(self, statement: str) -> str:
+    return input(statement)
+
+  def numberInputstatement(self, statement: str) -> int:
+    response = ""
+    while type(response) != int:
+      try:
+       response = int(input(statement))
+      except:
+        response = ""
+    return response
+
+  def boolInputstatement(self, statement: str) -> bool:
+    response = ""
+    while (response != 'y' and response != 'n'):
+      response = input(statement).lower()
+    return True if response == 'y' else False 
