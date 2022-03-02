be0bf2f508966e403201dca61037cf5faceafcc9
Craig <CMPeoples3@gmail.com>
2022-03-01 18:45:15 -0800

    yahtzee game object implementations: decide winner and run Game

--- v2/Yahtzee.py
+++ v2/Yahtzee.py
@@ -1,16 +1,16 @@
-import Player
+import Player, Turn, GameCommunication as gc
 
-
-class Yahtzee(self):
-  def __init__(self):
+class Yahtzee():
+  def __init__(self, gameCommunication:gc):
     self.self = self
-    self.players = self.getPlayers(self)
-    self.rounds = self.getRounds(self)
-    self.startGame(self)
+    self.gc = gameCommunication
+    self.players = self.getPlayers()
+    self.rounds = self.getRounds()
+    self.startGame()
 
   def getPlayers(self) -> list:
     players = []
-    player_names = input("What are players names? First names and put space in between \n")
+    player_names = self.gc.stringInputstatement("What are players names? First names and put space in between \n")
     player_names = player_names.split()
     for player in player_names:
         player = Player.Player(player, 0, ["One", "Two", "Three",
@@ -22,15 +22,34 @@ class Yahtzee(self):
     return players
 
   def getRounds(self) -> list:
-    rounds = ""
-    while (type(rounds) != int or rounds < 0 or rounds > 13):
-      rounds = input("How many rounds would you like to play? ")
+    rounds = -1
+    while (rounds < 0 or rounds > 13):
+      rounds = int(self.gc.numberInputstatement("How many rounds would you like to play? "))
     return rounds
 
   def startGame(self) -> None:
     turn = 0
     while turn < self.rounds:
-        # for player in self.players:
-        #     player.TakeTurn()
+        #Turn.Turn()
+        for player in self.players:
+          # print('p',player)
+          turnInt = Turn.Turn(player, 5, self.gc)
+          player.updateScore(turnInt.handleTurn())
         turn += 1
+    [print(player.name, player.score) for player in self.players]
+    self.decideWinner()
     return None
+
+  def decideWinner(self):
+    leader = ""
+    leaderScore = 0
+    for player in self.players:
+      if player.score > leaderScore:
+        leader = player.name
+        leaderScore = player.score
+    
+    print(f"WINNER {leader} with a score of {leaderScore}")
+    return None
+
+
+a = Yahtzee(gc.GameCommunication())
